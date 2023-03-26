dfcbed792e985f2cab28cb79b707ceb98528c00b
Felipe Gabriel Brezinski <felipegabrielbrezinski@gmail.com>
2023-03-26 20:00:43 -0300

    Aula 001, 002 e 003

--- javaViradoNoJiraya/untitled/src/Aula003.java
+++ javaViradoNoJiraya/untitled/src/Aula003.java
@@ -1,7 +1,7 @@
 public class Aula003 {
 
     public static void main(String[] args) {
-
+        /*
         //Operadores aritimeticos: = - * /
         int numero1 = 10;
         int numero02 = 20;
@@ -25,7 +25,44 @@ public class Aula003 {
         System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
         System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
         System.out.println("isDezIgualQueVinte "+isDezIgualVinte);
-        System.out.println("isDezDiferenteQueVinte "+isDezDiferentelVinte);
+        System.out.println("isDezDiferenteQueVinte "+isDezDiferentelVinte );
 
+        //  && (AND) (E)
+        // || (OR) (OU)
+        // Negaçao !
+
+        int idade = 29;
+        float salario = 3500F;
+        boolean  isDentroDaLeiMaiorQueTrinta = idade  >= 30 && salario >= 4612;
+        boolean  isDentroDaLeiMenorQueTrinta = idade  < 30 && salario >= 3381;
+
+        System.out.println(isDentroDaLeiMaiorQueTrinta);
+        System.out.println(isDentroDaLeiMenorQueTrinta);
+
+
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation =5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente >= valorPlaystation || valorTotalContaPoupanca >= valorPlaystation;
+        System.out.println(isPlaystationCincoCompravel);
+
+        // Operadores de atribuição: = += -= *= /= %=
+        */
+        double bonus = 1800; //1800
+        bonus += 1000;//2800
+        bonus -= 1000;//1800
+        bonus *= 2;//1800
+        bonus /= 2;//1800
+        bonus %= 2;//1800
+        System.out.println(bonus);
+
+        //
+        int contador = 0;
+        contador += 1;
+        contador -= 1;
+        contador++;
+        contador--;
+        System.out.println(contador);
     }
 }
