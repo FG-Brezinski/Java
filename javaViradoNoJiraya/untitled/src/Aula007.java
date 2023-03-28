83d4363198d2e10f9c62de9f7d49de3d48f97b51
Felipe Gabriel Brezinski <felipegabrielbrezinski@gmail.com>
2023-03-27 21:42:34 -0300

    Aula 007 e 008

---
+++ javaViradoNoJiraya/untitled/src/Aula007.java
@@ -0,0 +1,23 @@
+public class Aula007 {
+    //EUR 0     EUR 34,712      9.70%
+    //EUR 34,713     EUR 68,507      37.35%
+    //EUR 68,508     49.50%
+    public static void main(String[] args) {
+        double salarioAnual = 45000;
+        double primeiraFaixa = 9.70 / 100;
+        double segundaFaixa = 37.35 / 100;
+        double terceiraFaixa = 49.50 / 100;
+        double valorDoImposto;
+
+        if (salarioAnual <= 34712){
+            valorDoImposto = salarioAnual * primeiraFaixa;
+        } else if (salarioAnual >= 34712 && salarioAnual <= 68507) {
+            valorDoImposto = salarioAnual * primeiraFaixa;
+        } else {
+            valorDoImposto = salarioAnual * primeiraFaixa;
+        }
+
+        System.out.println(valorDoImposto);
+
+    }
+}
---
+++ javaViradoNoJiraya/untitled/src/Aula008.java
@@ -0,0 +1,55 @@
+public class Aula008 {
+    public static void main(String[] args) {
+        //Estrutura condicional Switch - Case
+        // Imprima o dia da semana considerando 1 como domingo
+        byte dia = 7;
+        // char, int, byte, short, enum, String
+
+        switch (dia){
+            case 1:
+                System.out.println("domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-Feira");
+                break;
+            case 3:
+                System.out.println("Terça-Feira");
+                break;
+            case 4:
+                System.out.println("Quarta-Feira");
+                break;
+            case 5:
+                System.out.println("Quinta-Feira");
+                break;
+            case 6:
+                System.out.println("Sexta-Feira");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Opção Invalida");
+
+        }
+
+        char sexo = 'f';
+        switch (sexo){
+            case 'M':
+                System.out.println("Homem");
+                break;
+            case 'F':
+                System.out.println("Mulher");
+                break;
+            case 'm':
+                System.out.println("Homem");
+                break;
+            case 'f':
+                System.out.println("Mulher");
+                break;
+            default:
+                System.out.println("Opção Invalida");
+                break;
+        }
+
+    }
+}
