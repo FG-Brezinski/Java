5b7e498707299de418bd338b262e76c7335a06a0
Felipe Gabriel Brezinski <felipegabrielbrezinski@gmail.com>
2023-04-03 22:09:56 -0300

    Introdução a Classes - Orientação a Objetos

---
+++ javaViradoNoJiraya/untitled/src/javacore/introducaoclasses/dominio/Estudante.java
@@ -0,0 +1,10 @@
+package javacore.introducaoclasses.dominio;
+
+public class Estudante {
+        //Classes
+        //Agrupamento de coisas que dão origem a um objeto
+        public String nome;
+        public int idade;
+        public char sexo;
+
+}
