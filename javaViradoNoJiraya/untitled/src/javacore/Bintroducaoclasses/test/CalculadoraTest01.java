539ad1ef5cad2ebc7464be7c63febbe793eba50b
Felipe Gabriel Brezinski <felipegabrielbrezinski@gmail.com>
2023-04-17 22:34:51 -0300

    INtrodução Classes

---
+++ javaViradoNoJiraya/untitled/src/javacore/Bintroducaoclasses/test/CalculadoraTest01.java
@@ -0,0 +1,13 @@
+package javacore.Bintroducaoclasses.test;
+
+import javacore.Bintroducaoclasses.domonio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01");
+        calculadora.subtraiDoisNumeros();
+    }
+
+}
---
+++ javaViradoNoJiraya/untitled/src/javacore/Bintroducaoclasses/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package javacore.Bintroducaoclasses.test;
+
+import javacore.Bintroducaoclasses.domonio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10,20);
+    }
+}
